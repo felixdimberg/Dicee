50f44dce276dc60232155ac4bedd6c231d11d7ee
xulemeng <bbc.lmeng@gmail.com>
2019-11-19 12:39:35 +0100

    Change felix's stuff
    Signed-off-by: xulemeng <bbc.lmeng@gmail.com>

--- app/src/main/java/com/example/dicee/MainActivity.java
+++ app/src/main/java/com/example/dicee/MainActivity.java
@@ -35,8 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.dice5,
                 R.drawable.dice6,
         };
-        // please lord help me
-        // ok, i help you
+        // please nuguygu8fty
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
